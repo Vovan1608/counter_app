f265c1b4a68b0c08a692efe5803b3d61ed887c52
Vovan1608 <vovan1608@gmail.com>
2021-04-01 15:59:57 +0300

    added app

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
 
 const getElement = (selector) => document.querySelector(selector);
 
+const app = getElement("#app");
+const item = getElement(".item");
+const decrease = getElement(".decrease");
+const reset = getElement(".reset");
+const increase = getElement(".increase");
+
+const doDecrease = () => item.innerText--;
+const doReset = () => item.innerText = 0;
+const doIncrease = () => item.innerText++;
+
+const changeClass = () => {
+	if (item.innerText > 0) {
+		item.setAttribute("positive");
+	}
+ 
+	if (item.innerText < 0) {
+		item.setAttribute("class", "negative")
+	}
+}
+
+decrease.addEventListener("click", doDecrease);
+reset.addEventListener("click", doReset);
+increase.addEventListener("click", doIncrease);
