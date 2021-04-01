5d6cdcb2fffef955b70cbecf196dacc8469ef4d6
Vovan1608 <vovan1608@gmail.com>
2021-04-01 17:38:04 +0300

    added e.target.className

--- main.js
+++ main.js
@@ -5,24 +5,25 @@ const getElement = (selector) => document.querySelector(selector);
 const app = getElement("#app");
 const item = getElement(".item");
 
-const decrease = getElement(".decrease");
-const reset = getElement(".reset");
-const increase = getElement(".increase");
+// const decrease = getElement(".decrease");
+// const reset = getElement(".reset");
+// const increase = getElement(".increase");
 
-const doDecrease = () => item.innerText--;
-const doReset = () => item.innerText = 0;
-const doIncrease = () => item.innerText++;
+// const doDecrease = () => item.innerText--;
+// const doReset = () => item.innerText = 0;
+// const doIncrease = () => item.innerText++;
 
 const changeClass = (e) => {
-	if (e.target === "") {
-		item.setAttribute("positive");
+	if (e.target.className === "decrease") {
+		item.innerText--;
 	}
-
-	if (item.innerText < 0) {
-		item.setAttribute("class", "negative")
+	if (e.target.className === "increase") {
+		item.innerText++;
 	}
 }
 
-decrease.addEventListener("click", doDecrease);
-reset.addEventListener("click", doReset);
-increase.addEventListener("click", doIncrease);
+// decrease.addEventListener("click", doDecrease);
+// reset.addEventListener("click", doReset);
+// increase.addEventListener("click", doIncrease);
+
+app.addEventListener("click", changeClass);
